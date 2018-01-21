b8e0b4c9e982502d637f37818b70e61bf5f1e0aa
Ben Kovitz <bkovitz@indiana.edu>
2018-01-20 19:01:20 -0500

    Trying to figure out how to deploy to Clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject farg/pmatch "0.1.0-SNAPSHOT"
   :description "Clojure translation of Oleg Kiselyov's pmatch macro"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/bkovitz/pmatch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :deploy-repositories [["releases" :clojars]]
+  )
 
--- src/farg/pmatch.clj
+++ src/farg/pmatch.clj
@@ -176,7 +176,7 @@
   [& args] `(recur 0 ~@args))
 
 (defmacro pmatch-loop
-  "Same as pmatch but allows recursion analogous to 'loop'.  'bindings' is a
+  "Same as pmatch but allows recursion analogous to 'loop'. 'bindings' is a
   vector the same as with 'loop'. Each iteration of the loop pmatches on the
   value of the first variable defined in the bindings. Inside the pmatch-loop,
   pmatch-recur calls the loop recursively, providing new values for each of
